f76121dc7c65ecd7879809c5e28ccfb0929e804e
Kristofer Liljeblad <kristol@microsoft.com>
2019-05-06 13:11:31 +0200

    Initial Solution Setup

---
+++ src/Krist00fer.LightningRouter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Krist00fer.LightningRouter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
